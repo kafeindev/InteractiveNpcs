b749bd6dd50d5e3ddfb4473138b73fbdf8676753
kafein <kafein40@gmail.com>
2023-12-09 20:02:46 +0300

    Add new options to settings.yml.

--- src/main/java/dev/kafein/interactivenpcs/configuration/ConfigVariables.java
+++ src/main/java/dev/kafein/interactivenpcs/configuration/ConfigVariables.java
@@ -9,6 +9,9 @@ public final class ConfigVariables {
 
     public static final ConfigKey<String> OFFSET_SYMBOL = ConfigKey.of("%img_offset_-1%", "offset-symbol");
 
-    public static final ConfigKey<List<String>> SPEECH_FONTS = ConfigKey.of(ImmutableList.of(), "speech", "fonts");
-    public static final ConfigKey<String> SPEECH_BUBBLE_LINES_DEFAULT_COLOR = ConfigKey.of("<black>", "speech", "bubble", "lines-default-color");
+    public static final ConfigKey<List<String>> SPEECH_BUBBLE_LINES_FONTS = ConfigKey.of(ImmutableList.of(), "speech", "bubble-lines", "fonts");
+    public static final ConfigKey<String> SPEECH_BUBBLE_LINES_DEFAULT_COLOR = ConfigKey.of("<black>", "speech", "bubble-lines", "default-color");
+    public static final ConfigKey<Integer> SPEECH_BUBBLE_LINES_DEFAULT_OFFSET = ConfigKey.of(0, "speech", "bubble-lines", "default-offset");
+    public static final ConfigKey<String> SPEECH_BUBBLE_LINES_DEFAULT_IMAGE_SYMBOL = ConfigKey.of("%img_bubble%", "speech", "bubble-lines", "image", "default-symbol");
+    public static final ConfigKey<Integer> SPEECH_BUBBLE_LINES_DEFAULT_IMAGE_WIDTH = ConfigKey.of(0, "speech", "bubble-lines", "image", "default-width");
 }
